08f06b7ce56587e3939c6dea4a602fc24836a2e5
Cryolite <cryolite.indigo@gmail.com>
2021-10-07 21:33:12 +0900

    Add support for Si Jia Li Zhi (四家立直)

--- majsoul_rpa/presentation/match/event/liuju.py
+++ majsoul_rpa/presentation/match/event/liuju.py
@@ -2,6 +2,7 @@
 
 import datetime
 from typing import Optional
+from majsoul_rpa.presentation.presentation_base import InconsistentMessage
 from majsoul_rpa.presentation.match.event._base import EventBase
 
 
@@ -9,13 +10,15 @@ class LiujuEvent(EventBase):
     def __init__(self, data: object, timestamp: datetime.datetime) -> None:
         super(LiujuEvent, self).__init__(timestamp)
 
-        if data['type'] >= 2:
+        if data['type'] not in (1, 4):
             raise NotImplementedError(data['type'])
-        self.__type = (None, '九種九牌')[data['type']]
+        self.__type = (None, '九種九牌', None, None, '四家立直')[data['type']]
         if self.__type == '九種九牌':
             self.__seat = data['seat']
         else:
-            assert(data['seat'] == '')
+            if data['seat'] != 0:
+                raise InconsistentMessage(
+                    f'type = {data["type"]}, seat = {data["seat"]}')
             self.__seat = None
 
     @property
